50bdec148b8901643cfc6dc0a74922f464e05802
Fran3000vv <105230779+Fran3000vv@users.noreply.github.com>
2022-09-29 17:05:21 +0200

    Delete JuegoPuertas directory

--- JuegoPuertas/e.php
+++
@@ -1,8 +0,0 @@
-<html>
-    <head>
-
-    </head>
-    <body>
-        <center><h1>Enhorabuena de la Buena</h1></center>
-    </body>
-</html>
--- JuegoPuertas/m.php
+++
@@ -1,8 +0,0 @@
-<html>
-    <head>
-
-    </head>
-    <body>
-    <center><h1>Muerte</h1></center>
-    </body>
-</html>
